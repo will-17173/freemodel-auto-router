276c93961a0e977f9bd1d6dbfc7e3dda124f1cea
will-17173 <huangzhaojian@gmail.com>
2026-05-13 12:11:37 +0800

    chore: add cn() helper and configure @ path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -8,6 +9,12 @@ const host = process.env.TAURI_DEV_HOST;
 // https://vite.dev/config/
 export default defineConfig(async () => ({
   plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 
   optimizeDeps: {
     exclude: [],
